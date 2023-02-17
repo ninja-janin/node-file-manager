84ca193575551e0ea15b91566cadf9d3446bc93c
lukemags-07 <62930692+lukemags-07@users.noreply.github.com>
2023-02-17 15:29:18 +0800

    Add generate for overseas area data

--- js/generate-overseas.js
+++ js/generate-overseas.js
@@ -4,8 +4,8 @@ const { readFileSync } = require('fs');
 let area = ["america", "australia", "austria", "bali", "bangladesh", "beijing", "belgium", "boston", "brazil", "brisbane", "cairns", "cambodia", "canada", "cebu", "china", "croatia", "cuba", "czech", "dubai", "egypt", "fiji", "florence", "france", "germany", "goldcoast", "greece", "guam", "hainan", "hawaii", "hongkong", "hungary", "india", "indonesia", "italy", "jordan", "kenya", "korea", "laos", "lasvegas", "losangeles", "macau", "malaysia", "maldives", "malta", "mauritius", "mexico", "milano", "mongolia", "morocco", "myanmar", "nepal", "netherlands", "newcaledonia", "newyork", "newzealand", "orlando", "palau", "peru", "philippines", "phuket", "poland", "portugal", "rome", "russia", "saipan", "sandiego", "sanfrancisco", "seattle", "seychelles", "shanghai", "singapore", "southafrica", "spain", "srilanka", "switzerland", "sydney", "tahiti", "taiwan", "thailand", "toronto", "turkey", "uae", "united-kingdom", "vancouver", "venice", "vietnam"]
 
 function createAreaData(data, areaName) {
-    console.log(`data-overseas-new/${areaName}.json`)
-    fs.writeFile(`data-overseas-new/${areaName}.json`, JSON.stringify(data, null, 4), function (err) {}); 
+    console.log(`data-overseas/${areaName}.json`)
+    fs.writeFile(`data-overseas/${areaName}.json`, JSON.stringify(data, null, 4), function (err) {}); 
 }
 
 area.forEach((areaName) => {
