89e74443fd51194dcaa33b9626cd3737a6a7b3ba
lukemags-07 <62930692+lukemags-07@users.noreply.github.com>
2023-02-17 12:00:51 +0800

    Add js for index generate

---
+++ js/async-fs.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+try {
+  fs.writeFileSync('data.txt', 'Created in Node.js');
+  console.log("File written successfully");
+} catch(e) {
+  console.log(e);
+}
+
+try {
+  const data = fs.readFileSync('data.txt', 'utf8');
+  console.log(data);
+} catch(e) {
+  console.log(e);
+}
